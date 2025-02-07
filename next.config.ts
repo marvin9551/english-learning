ac668a50545d8f2ecaf053a514c2a89972b31d87
marvin <hao.min1995@foxmail.com>
2025-02-07 17:48:31 +0800

    修改next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,12 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  eslint: {
+    ignoreDuringBuilds: true,  // 构建时忽略 ESLint 错误
+  },
+  typescript: {
+    ignoreBuildErrors: true,  // 构建时忽略 TypeScript 类型检查错误
+  },
 };
 
 export default nextConfig;
